e26105131d41ec94113352e156742eda7723d91c
ScotchCarbTTV <70674628+ScotchCarbTTV@users.noreply.github.com>
2023-07-12 17:01:43 +0800

    Day 2 Commit
    Worked up to the end of 'Unit Selected Visual, Singleton' video hell yaah

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,29 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator;
+
     private Vector3 targetPosition;
-    
+
+    private void Awake()
+    {
+        targetPosition = transform.position;
+    }
 
     private void Update()
     {
+        
+
         float stoppingDistance = 0.1f;
         if (Vector3.Distance(transform.position, targetPosition) >= stoppingDistance)
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, rotateSpeed * Time.deltaTime);
+
+            unitAnimator.SetBool("isWalking", true);
+        }
+        else
+        {
+            unitAnimator.SetBool("isWalking", false);
         }
 
-        if (Input.GetMouseButtonDown(1))
-        {
-            Move(MouseWorld.GetPosition());
-        }
+       
     }
 
-    private void Move(Vector3 targetPosition)
+    public void Move(Vector3 targetPosition)
     {
         //unit will move towards the target position
 
